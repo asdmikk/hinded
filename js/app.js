f4b41fcbbca457bedd7d7e11ff23e4f596a818c4
Mikk Kärner <k2rnermikk@gmail.com>
2016-03-19 14:45:39 +0200

    More stuff

--- js/app.js
+++ js/app.js
@@ -2,12 +2,27 @@ var app = angular.module('GradesApp', []);
 
 app.run(function ($rootScope) {
 	$rootScope.currenUser;
+	$rootScope.assignments = ['1 KT', '2 KT', 'Lisapunktid', 'Eksam'];
+	var grades = {
+		'1 KT': 1,
+		'2 KT': 2,
+		'Lisapunktid': 0,
+		'Eksam': 5
+	};
 	$rootScope.users = [
+		{id: 312123, email: 'm@m.mm', firstName: 'Mikk', lastName: 'K', pass: 's', status: 'student', code: 123123, grades: grades},
+		{id: 312133, email: 'm@m1.mm', firstName: 'Paul', lastName: 'K', pass: 's', status: 'student', code: 123523, grades: grades},
+		{id: 312123, email: 'm@m2.mm', firstName: 'Jeesus', lastName: 'K', pass: 's', status: 'student', code: 123153, grades: grades},
+		{id: 312223, email: 'm@m3.mm', firstName: 'Roberta', lastName: 'K', pass: 's', status: 'student', code: 126123, grades: grades},
+		{id: 312123, email: 'm@m4.mm', firstName: 'Roberto', lastName: 'K', pass: 's', status: 'student', code: 123723, grades: grades},
+		{id: 312343, email: 'm@m5.mm', firstName: 'Joujou', lastName: 'K', pass: 's', status: 'student', code: 153123, grades: grades},
+		{id: 312323, email: 'm@m6.mm', firstName: 'Mikk', lastName: 'K', pass: 's', status: 'student', code: 123723, grades: grades},
 		{
-			email: 'm@m.mm', firstName: 'Mikk', lastName: 'K', pass: 's', status: 'student', code: 123123
-		},{
-			email: 't@t.tt', firstName: 'Teacher', lastName: 'T', pass: 't', status: 'teacher'
+			id: 123421, email: 't@t.tt', firstName: 'Teacher', lastName: 'T', pass: 't', status: 'teacher'
 		}];
+	$rootScope.assignments = ['1 KT', '2 KT', 'Lisapunktid', 'Eksam'];
+
+	$rootScope.currenUser = $rootScope.users[1];
 });
 
 app.controller('AppCtrl', function ($rootScope) {
@@ -73,6 +88,7 @@ app.controller('RegisterCtrl', function ($rootScope) {
 			this.error = true;
 		}
 		if (this.infoSet) {
+			this.user.id = Math.random();
 			$rootScope.users.push(this.user);
 			$rootScope.currenUser = this.user;
 			$rootScope.$broadcast('userChanged');
@@ -92,6 +108,12 @@ app.controller('RegisterCtrl', function ($rootScope) {
 	}
 });
 
-app.controller('MainCtrl', function ($rootScope) {
+app.controller('TableCtrl', function ($rootScope) {
+	this.students = $rootScope.users.filter(u => u.status === 'student');
+	this.assignments = $rootScope.assignments;
+	this.headers = ['Name', 'Student code'].concat(this.assignments);
 
+	this.edit = function (student) {
+		console.log(student)
+	}
 });
